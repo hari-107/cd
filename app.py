208f82cfcc78fe1d25cd03a379e750d049a05552
hari <hariprasath90047@gmail.com>
2026-08-22 08:50:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Weather Dashboard</title>
+<title>kanguva Dashboard</title>
 <style>
 * { box-sizing: border-box; }
 body {
